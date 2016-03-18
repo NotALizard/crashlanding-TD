a016f57d6b27f5a9a89b2f580fb9798645458228
NotALizard <gcb140330@utdallas.edu>
2016-03-18 15:14:12 -0500

    Implemented a crosshair that follows the mouse cursor

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	
+	void Update () {
+        this.transform.position = new Vector3(10 * (Input.mousePosition.x - (Screen.width / 2)) / Screen.height, 10 * (Input.mousePosition.y - (Screen.height / 2)) / Screen.height, 0);
+	}
+
+    public Vector3 getPos()
+    {
+        return this.transform.position;
+    }
+}
