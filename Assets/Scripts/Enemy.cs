a7e52b926e3635f371b4d7c7d49d8e46c7654ef2
NotALizard <gcb140330@utdallas.edu>
2016-06-13 23:15:57 -0500

    Created an enemy base class and started work on Roller enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    protected float maxHealth;
+    protected float health;
+    protected float damage;
+    protected float speed;
+    protected bool facingRight;
+    protected int moveDirection;
+    protected Rigidbody2D myBody2D;
+
+    public virtual void Init(float maxHP, float dam, float spd)
+    {
+        maxHealth = maxHP;
+        damage = dam;
+        speed = spd;
+        health = maxHealth;
+        myBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Hurt(float dam)
+    {
+        health = health - dam;
+        if (health <= 0)
+            Kill();
+    }
+
+    private void Kill()
+    {
+        //todo
+    }
+
+    public void Flip()
+    {
+        if (facingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            facingRight = false;
+        }
+        else if (!facingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            facingRight = true;
+        }
+    }
+}
