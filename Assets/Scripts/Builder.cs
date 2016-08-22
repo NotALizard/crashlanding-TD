010d751dc52867d68a5d822f358bcf60b5a67ba3
NotALizard <gcb140330@utdallas.edu>
2016-08-22 15:17:31 -0500

    Started implementation of building upgrade system

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Builder : MonoBehaviour {
+
+    public static void basicWall(Building b)
+    {
+        b.leftOpt = basicGun;
+
+    }
+
+    public static void basicGun(Building b)
+    {
+
+    }
+    
+}
--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,6 +5,17 @@ public class Building : MonoBehaviour {
     protected float maxHealth;
     protected float health;
 
+    //Upgrades
+    public delegate void BuildDel(Building b);
+    public BuildDel leftOpt;
+    public BuildDel topOpt;
+    public BuildDel rightOpt;
+    public BuildDel sellOpt;
+
+
+
+    private int[] upgradeCosts = new int[4];
+
     public void Hurt(float dam)
     {
         health = health - dam;
@@ -16,4 +27,12 @@ public class Building : MonoBehaviour {
     {
         //soon
     }
+
+    public void SetCosts(int left, int top, int right, int sell)
+    {
+        upgradeCosts = new int[4]{left, top, right, sell};
+    }
+
+    public int[] GetCosts() { return upgradeCosts; }
+
 }
