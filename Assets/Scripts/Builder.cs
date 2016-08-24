0b1e338eba29dd8478f56fef83c7779103179378
NotALizard <gcb140330@utdallas.edu>
2016-08-23 20:52:31 -0500

    Started implementing basic upgrades

--- Assets/Scripts/Builder.cs
+++ Assets/Scripts/Builder.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class Builder : MonoBehaviour {
 
-    public static void basicWall(Building b)
+    //#### CONSTANTS - BUILDING COSTS ####
+    const int ScaffoldCost = 20;
+    const int BasicWallCost = 100;
+    const int BasicTurretCost = 100;
+    const int BasicGenCost = 100;
+    const int RapidTurretCost = 150;
+    const int MortarTurretCost = 175;
+    const int LaserTurretCost = 200;
+    const int HeavyWallCost = 150;
+    const int ReflectWallCost = 175;
+    const int RefractWallCost = 200;
+    const int NecroGenCost = 150;
+    const int RepairGenCost = 175;
+    const int BuffGenCost = 200;
+    //#####################################
+
+
+
+
+    public static int Scaffold(Building b)
     {
-        b.leftOpt = basicGun;
+        b.Init(Mathf.Infinity, "scaffold");
+        b.leftOpt = BasicGun;
+        b.topOpt = BasicGen;
+        b.rightOpt = BasicWall;
+        b.sellOpt = null;
+        b.gameObject.tag = "Scaffold";
+
+        return (ScaffoldCost);
+    }
+
+    public static int BasicWall(Building b)
+    {
+
 
     }
 
-    public static void basicGun(Building b)
+    public static int BasicGun(Building b)
+    {
+
+    }
+
+    public static int BasicGen(Building b)
     {
 
     }
--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -6,7 +6,7 @@ public class Building : MonoBehaviour {
     protected float health;
 
     //Upgrades
-    public delegate void BuildDel(Building b);
+    public delegate int BuildDel(Building b);
     public BuildDel leftOpt;
     public BuildDel topOpt;
     public BuildDel rightOpt;
@@ -15,6 +15,11 @@ public class Building : MonoBehaviour {
 
 
     private int[] upgradeCosts = new int[4];
+
+    public void Init(float maxHP, string sprite)
+    {
+
+    }
 
     public void Hurt(float dam)
     {
@@ -25,7 +30,7 @@ public class Building : MonoBehaviour {
 
     private void Kill()
     {
-        //soon
+        sellOpt(this);
     }
 
     public void SetCosts(int left, int top, int right, int sell)
@@ -34,5 +39,7 @@ public class Building : MonoBehaviour {
     }
 
     public int[] GetCosts() { return upgradeCosts; }
+
+    
 
 }
