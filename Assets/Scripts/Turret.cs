f9976a5ce128952fd4a508d9e2ad150c8a96c6e1
NotALizard <gcb140330@utdallas.edu>
2016-08-25 12:32:07 -0500

    Began work on implementing turrets

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+    protected float maxRange = 25;
+    protected GameObject target;
+
+    protected void Aim(Vector2 enemyPos)
+    {
+        //todo
+    }
+	
+
+    GameObject FindUrgentEnemy()
+    {
+        GameObject bestTarget = null;
+        float distanceFromLeft = Mathf.Infinity;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(GameObject e in enemies)
+        {
+            if(Mathf.Abs(Vector2.Distance(this.transform.position, e.transform.position)) <= maxRange && e.transform.position.x < distanceFromLeft)
+            {
+                bestTarget = e;
+                distanceFromLeft = bestTarget.transform.position.x;
+            }
+        }
+        return bestTarget;
+
+
+
+    }
+}
